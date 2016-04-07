4f129b602ad69fdb47b756ea7bfc76c088872b15
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-04-07 14:37:20 +0900

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   root 'zadankai#home'
   get 'zadankai/about'
+  get 'users/index'
+  get 'users/show'
 
   devise_for :users, :controllers => {
     :sessions      => "users/sessions",
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
     :passwords     => "users/passwords",
     :omniauth_callbacks => "users/omniauth_callbacks"
   }
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
